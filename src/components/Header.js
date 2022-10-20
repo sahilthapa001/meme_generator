15ffb6287953d64f6c88f43399bac7a229f954c8
sahil <91470287+sahilthapa001@users.noreply.github.com>
2022-10-20 21:58:10 +0545

    now it is :)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
 import React from "react";
+import troll from "../images/troll-face.png";
 
 export default function Header() {
 	return (
 		<header className="header">
-			<img
-				src="./images/troll-face.png"
-				alt="troll"
-				className="header--image"
-			/>
+			<img src={troll} alt="troll" className="header--image" />
 
 			<h2 className="header--title">Meme Generator</h2>
 			<h4 className="header--project">React Course - Project 3</h4>
